460ed61cde5b8c6e48782cd744af8ed4a5dac299
Jon Sadowski <jwsadowski@gmail.com>
2022-11-21 17:54:06 -0600

    fourth commit

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,24 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department(
-    id INT 
-    name VARCHAR(30)
+    id INT,
+    name VARCHAR(30),
     PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-    id INT 
-    title VARCHAR(30)
-    salary DECIMAL
-    department_id INT
+    id INT, 
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
     PRIMARY KEY(id)
-)
+);
 
 CREATE TABLE employee (
-    id INT 
-    first_name VARCHAR(30)
-    last_name VARCHAR(30)
-    role_id INT
-    manager_id INT
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
     PRIMARY KEY(id)
-)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,10 +5,10 @@ INSERT INTO department (id, name)
 VALUES (2, "Engineering");
 
 INSERT INTO department (id, name)
-VALUES (3, "Finance")
+VALUES (3, "Finance");
 
 INSERT INTO department (id, name)
-VALUES (4, "Legal")
+VALUES (4, "Legal");
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES (1, "Salesperson", 80000, 1);
@@ -31,24 +31,24 @@ VALUES (6, "Legal team lead", 250000, 4);
 INSERT INTO role (id, title, salary, department_id)
 VALUES (7, "Lawyer", 190000, 4);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (2, "Mike", "Chan", 1, 1);
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
+VALUES ( "Mike", "Chan", 1, 1);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (3, "Ashley", "Rodriguez", 2, NULL);
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
+VALUES ( "Ashley", "Rodriguez", 2, NULL);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (2, "Kevin", "Tupik", 3, 2);
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
+VALUES ( "Kevin", "Tupik", 3, 2);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (2, "Kunal", "Singh", 4, NULL);
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
+VALUES ( "Kunal", "Singh", 4, NULL);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (2, "Malia", "Brown", 5, 3);
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
+VALUES ( "Malia", "Brown", 5, 3);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (2, "Sarah", "Lourd", 6, NULL);
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
+VALUES ( "Sarah", "Lourd", 6, NULL);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (2, "Tom", "Allen", 7, 4);
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
+VALUES ( "Tom", "Allen", 7, 4);
 
